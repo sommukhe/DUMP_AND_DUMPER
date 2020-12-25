e38a424d4ee4c89b0aea2b34d3877e5c205714cb
Soma <41096407+sommukhe@users.noreply.github.com>
2020-12-25 18:40:25 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var paper, paperSprite, ground;
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+	engine = Engine.create();
+	world = engine.world;
+	//Create the Bodies Here.
+    paper = new Paper(100,400,50);
+	ground = new Ground(200,500,2000,20)
+	side1Sprite=createSprite(width/2 + 320, height-215, 110,10);
+	side1Sprite.shapeColor = "red"
+	side2Sprite=createSprite(width/2 + 260, height-240, 10,60);
+	side2Sprite.shapeColor = "red"
+	side3Sprite=createSprite(width/2 + 380, height-240, 10,60);
+	side3Sprite.shapeColor = "red"
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  
+  background(0);
+  
+  paper.display();
+  ground.display();
+  drawSprites();
+ 
+}
+
+
+function keyPressed(){
+	if (keyCode===UP_ARROW){
+		Matter.Body.applyForce(paper.body,paper.body.position,{x:95,y:-95});
+	}
+}
